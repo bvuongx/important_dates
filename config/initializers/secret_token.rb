43eec18270b19a8c1fdd55e71a094f99195aa933
bvuongx <bvuong@gmail.com>
2013-07-16 14:14:41 -0700

    Important Dates - App a day

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImportantDates::Application.config.secret_token = '3da47b60a06c942686afe0d42f152fa297a02fd1a01cc4f28f65cbd64ab8e9451f34a51ced680382089e191534a25803af4e53a76d88abea435c4e863e306209'
